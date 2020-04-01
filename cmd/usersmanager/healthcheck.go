09bf168fe788d13d70ed8866233d815d2c16433a
Gokul T P <tp.gokul@gmail.com>
2020-04-01 22:33:42 +0530

    initial framework

---
+++ cmd/usersmanager/healthcheck.go
@@ -0,0 +1,10 @@
+package main
+
+var (
+	// Version is the build version
+	Version string
+	// MinVersion is the latest git commit hash
+	MinVersion string
+	// BuildTime is the time at which this build is made
+	BuildTime string
+)
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/gokultp/aution-bidder
+
+go 1.12
